e84a4c9075663d0e373718a90c8c69f55a53c576
Rahu206 <81007551+Rahu206@users.noreply.github.com>
2021-07-27 15:00:22 +0530

    UC1-added method for checking First name

---
+++ UserValidation.java
@@ -0,0 +1,17 @@
+package com.bridgelabz;
+
+import java.util.regex.Pattern;
+
+public class UserValidation {
+	
+	private final String REGEX_FOR_FIRST_NAME = "^[A-Z]{1}[a-z]{2,}$";
+	
+	public boolean validateFirstName(String firstname) {
+	
+ 	return Pattern.matches(REGEX_FOR_FIRST_NAME, firstname);
+	}
+ 	
+ 	
+ 	
+
+}
