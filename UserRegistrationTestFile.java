938158e3d49588859dc76616212e8790a6d455d7
Rahu206 <81007551+Rahu206@users.noreply.github.com>
2021-07-27 20:44:12 +0530

    UC3-added Test class for enter a valid email

---
+++ UserRegistrationTestFile.java
@@ -0,0 +1,50 @@
+package com.bridgelabz;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class UserRegistrationTestFile {
+
+	private UserValidation userValidation;
+
+	@Before
+	public void setup() {
+		userValidation = new UserValidation();
+
+	}
+
+	@Test
+	public void testFirstName() {
+		assertEquals(true, userValidation.validateFirstName("Rahul"));
+
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void testFirstNameNegativeScenerio() {
+		assertEquals(true, userValidation.validateFirstName(null));
+	}
+
+	@Test
+	public void testLastName() {
+		assertEquals(true, userValidation.validateLastName("Deshpande"));
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void testLastNameNameNegativeScenerio() {
+		assertEquals(true, userValidation.validateLastName(null));
+	}
+
+	@Test
+	public void testEmail() {
+		assertEquals(true, userValidation.validateEmail("rahuldeshpande93@gmail.com"));
+	}
+	
+	@Test(expected = NullPointerException.class)
+	public void testEmailNegativeScenerio() {
+		assertEquals(true, userValidation.validateEmail(null));
+	}
+	
+
+}
