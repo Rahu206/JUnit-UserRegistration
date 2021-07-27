34531793f82c02b5bb3fe0a1147a6815db07cbc0
Rahu206 <81007551+Rahu206@users.noreply.github.com>
2021-07-27 17:02:12 +0530

    UC5 -added method for checking all type of password

---
+++ UserValidation.java
@@ -0,0 +1,39 @@
+package com.bridgelabz;
+
+import java.util.regex.Pattern;
+
+public class UserValidation {
+
+	private final String REGEX_FOR_FIRST_NAME = "^[A-Z]{1}[a-z]{2,}$";
+	private final String REGEX_FOR_LAST_NAME = "^[A-Z]{1}[a-z]{2,}$";
+	private final String REGIX_FOR_Email = "^[a-zA-Z0-9+.-]+@[a-zA-Z0-9.]+$";
+	private final String regex_for_mobileNumber = "^[0-9+ ]{0,3}[0-9]{0,10}";
+	private final String regex_for_password = "[A-Za-z0-9@_#$%!]{8,}";
+
+	public boolean validateFirstName(String firstname) {
+
+		return Pattern.matches(REGEX_FOR_FIRST_NAME, firstname);
+	}
+
+	public boolean validateLastName(String lastName) {
+
+		return Pattern.matches(REGEX_FOR_LAST_NAME, lastName);
+
+	}
+
+	public boolean validateEmail(String email) {
+		return Pattern.matches(REGIX_FOR_Email, email);
+
+	}
+
+	public boolean validateMobileNumber(String mobileNumber) {
+		return Pattern.matches(regex_for_mobileNumber, mobileNumber);
+
+	}
+
+	public boolean validatePassword(String password) {
+		return Pattern.matches(regex_for_password, password);
+
+	}
+
+}
